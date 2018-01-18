3c3b5276c9f43b0e32c5b34946f9916734151fcd
aTonyXiao <tony.xiao95@icloud.com>
2018-01-18 14:50:38 -0800

    question1 & junit

---
+++ src/hw1/DatabaseManager.java
@@ -0,0 +1,20 @@
+package hw1;
+
+/**
+ * Created by TonyXiao on 1/18/18.
+ */
+
+public class DatabaseManager {
+    private static DatabaseManager instance = null;
+    private DatabaseManager() {
+        System.out.println("Instance Created");
+    }
+    public static DatabaseManager TheDatabaseMangaer() {
+        if(instance == null)
+            instance = new DatabaseManager();
+        else
+            System.out.println("Previously Created instance returned");
+
+        return instance;
+    }
+}
---
+++ src/hw1/LibraryBook.java
@@ -0,0 +1,7 @@
+package hw1;
+
+/**
+ * Created by TonyXiao on 1/18/18.
+ */
+public class LibraryBook {
+}
