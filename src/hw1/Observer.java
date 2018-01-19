3d54a9136d166cf94a18b8eb219441c2d3398211
aTonyXiao <tony.xiao95@icloud.com>
2018-01-19 11:23:24 -0800

    thoughts for Q3

---
+++ src/hw1/Observer.java
@@ -0,0 +1,13 @@
+package hw1;
+
+/**
+ * Created by TonyXiao on 1/18/18.
+ */
+public interface Observer {
+}
+
+
+//Q3. LibraryBook is the subject.
+// ConcreteObservers: Dest Observer, Source Observer
+// Source only see previous states
+// Dest see currently states
